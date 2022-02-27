bbbc7fc61132af28df8464895aa2c3f00a8e5060
Gayrat-Dev <97515743+Gayrat-Dev@users.noreply.github.com>
2022-02-27 13:08:17 +0500

    main change

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel-block').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
